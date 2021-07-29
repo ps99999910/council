75dc23ca66cc3b0fcb3e0352abbccc5d02974689
= <=>
2021-07-29 09:13:53 +0100

    Updating...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
                 </div>
             @endif
             <div>Stable with unit test</div>
+
 <!--
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
